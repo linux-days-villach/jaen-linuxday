755358351cea19579370a8fa76d53cebc876c36d
petute <daniel.petutschnigg@gmail.com>
2021-10-05 02:52:13 +0200

    chore: finish cleanup
    - Cleanup jaen-config.js
    - Cleanup jaen-pages.json

--- jaen-config.js
+++ jaen-config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  remote: 'linux-day-villach/jaen-linuxday',
+  remote: 'linux-days-villach/jaen-linuxday',
   initialHideUI: false, // optional hide UI on load
   plugins: {
     pages: {
       resolve: require('@snek-at/jaen-pages/jaen-register'),
-      templates: [require('./src/templates/SamplePage.tsx')]
+      templates: []
     }
   }
 }
